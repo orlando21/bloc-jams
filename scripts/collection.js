ac1f8b008603d9bd3b15f39d4b5ad2b5c045609d
Greg Babb <info@textlogic.ch>
2016-02-22 20:29:52 +0100

    Checkpoint 24 collections: completed

---
+++ scripts/collection.js
@@ -0,0 +1,27 @@
+var collectionItemTemplate =
+     '<div class="collection-album-container column fourth">'
+   + '  <img src="assets/images/album_covers/01.png"/>'
+   + '  <div class="collection-album-info caption">'
+   + '    <p>'
+   + '      <a class="album-name" href="/album.html"> The Colors </a>'
+   + '      <br/>'
+   + '      <a href="/album.html"> Pablo Picasso </a>'
+   + '      <br/>'
+   + '      X songs'
+   + '      <br/>'
+   + '    </p>'
+   + '  </div>'
+   + '</div>'
+   ;
+
+ window.onload = function() {
+     // #1
+     var collectionContainer = document.getElementsByClassName('album-covers')[0];
+     // #2
+     collectionContainer.innerHTML = '';
+ 
+     // #3
+     for (var i = 0; i < 12; i++) {
+         collectionContainer.innerHTML += collectionItemTemplate;
+     }
+ }
