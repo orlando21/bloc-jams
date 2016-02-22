8fae00dd6929f25e367a2532874d4192cb9b083f
Greg Babb <info@textlogic.ch>
2016-02-22 18:54:17 +0100

    Checkpoint 23 DOM Events: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,45 +1,36 @@
-                 var animatePoints = function() {
-                     
-                    var revealPoint = function(index) {
-                        
-                        points[index].style.opacity = 1;
-                        points[index].style.transform = "scaleX(1) translateY(0) rotate(360deg)";
-                        points[index].style.msTransform = "scaleX(1) translateY(0) rotate(360deg)";
-                        points[index].style.WebkitTransform = "scaleX(1) translateY(0) rotate(360deg)"; 
-                        
-                    }
+var pointsArray = document.getElementsByClassName('point');
 
- 
-                 /*var revealFirstPoint = function() {
-                     points[0].style.opacity = 1;
-                     points[0].style.transform = "scaleX(1) translateY(0) rotate(360deg)";
-                     points[0].style.msTransform = "scaleX(1) translateY(0) rotate(360deg)";
-                     points[0].style.WebkitTransform = "scaleX(1) translateY(0) rotate(360deg)";
-                 };
- 
-                 var revealSecondPoint = function() {
-                     points[1].style.opacity = 1;
-                     points[1].style.transform = "scaleX(1) translateY(0) rotate(360deg)";
-                     points[1].style.msTransform = "scaleX(1) translateY(0) rotate(360deg)";
-                     points[1].style.WebkitTransform = "scaleX(1) translateY(0) rotate(360deg)";
-                 };
- 
-                 var revealThirdPoint = function() {
-                     points[2].style.opacity = 1;
-                     points[2].style.transform = "scaleX(1) translateY(0) rotate(360deg)";
-                     points[2].style.msTransform = "scaleX(1) translateY(0) rotate(360deg)";
-                     points[2].style.WebkitTransform = "scaleX(1) translateY(0) rotate(360deg)";
-                 };
- 
-                 revealFirstPoint();
-                 revealSecondPoint();
-                 revealThirdPoint();*/
-                 
-                var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
                      
-                for (i=0;i<points.length;i++){       
-                    revealPoint(i);                       
-                }
+    var revealPoint = function(index) {
+                        
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0) rotate(360deg)";
+        points[index].style.msTransform = "scaleX(1) translateY(0) rotate(360deg)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0) rotate(360deg)";                   
+    }
+                                
+                     
+    for (i=0;i<points.length;i++){       
+        revealPoint(i);                       
+        }
             
                      
-             }; 
+}; 
+
+window.onload = function() {
+    
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+        }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);   
+            }
+        });
+    
+}
