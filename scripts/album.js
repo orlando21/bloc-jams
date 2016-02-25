130767c11dcb6f38be4a9a6b46f3be267bfc4954
Greg Babb <info@textlogic.ch>
2016-02-25 18:03:19 +0100

    Checkpoint 26 completed

--- scripts/album.js
+++ scripts/album.js
@@ -49,7 +49,7 @@
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
-      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
@@ -82,10 +82,31 @@ var setCurrentAlbum = function(album) {
     
  };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
 window.onload = function() {
     
     setCurrentAlbum(albumPicasso);
+    
+    songListContainer.addEventListener('mouseover', function(event) {
+        // Only target individual song rows during event delegation
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;   
+         }
+     });
+    
+    for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+        // Selects first child element, which is the song-item-number element
+        this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+         });
+     }
+
+    
     albums = [albumPicasso, albumMarconi, albumZeppelin];
     var next = 1;
     albumImage.addEventListener('click', function(event){
