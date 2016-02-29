1007681c41fbdab889039767dd47df5ed5b0c788
Greg Babb <info@textlogic.ch>
2016-02-29 20:33:08 +0100

    Completed checkpoint 29

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,35 +1,25 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var revealPoint = function(point) {
-                        
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0) rotate(360deg)";
-    point.style.msTransform = "scaleX(1) translateY(0) rotate(360deg)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0) rotate(360deg)";                   
-    }
-
-var animatePoints = function(points) {
- 
-/*    for (i=0;i<points.length;i++){       
-        revealPoint(i);                       
-        } */
-    forEach(points, revealPoint);
-                     
-}; 
-
-window.onload = function() {
-    
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
-        }
-    
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);   
-            }
-        });
-    
+var animatePoints = function() {  
+     var revealPoint = function() {
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+     }; 
+$.each($('.point'), revealPoint);
 }
+
+ $(window).load(function() {
+    
+     if ($(window).height() > 950) {
+         animatePoints();
+     }
+    
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+    $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
+            }
+    });
+    
+});
