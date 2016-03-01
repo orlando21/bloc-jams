761805eea4ce76e750336ba92c26dfc2abe65de5
Greg Babb <info@textlogic.ch>
2016-03-01 17:59:04 +0100

    Completed checkpoint 30

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+     var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -13,15 +14,18 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>'
    ;
+     return $(template);
+    };
 
- window.onload = function() {
+$(window).load(function() {
      // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    var $collectionContainer = $(".album-covers");
      // #2
-     collectionContainer.innerHTML = '';
+    $collectionContainer.empty();
  
      // #3
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+        var $newThumbnail = buildCollectionItemTemplate();
+        $collectionContainer.append($newThumbnail);
      }
- }
+});
